17bd9552f6b1d7803ead40544f3baf4dfc5948b8
unknown <rasmuskirjanen@gmail.com>
2020-11-22 20:29:51 +0200

    hello world

---
+++ components/Btn.vue
@@ -0,0 +1,30 @@
+<template>
+<button class="button" :class="{
+  'is-primary': primary,
+  'is-info': info,
+  'is-link': link,
+  'is-success': success,
+  'is-warning': warning,
+  'is-danger': danger
+
+
+}"><slot></slot></button>
+</template>
+
+<script>
+export default {
+name: "Btn",
+  props: {
+    primary: Boolean,
+    info: Boolean,
+    link: Boolean,
+    success: Boolean,
+    warning: Boolean,
+    danger: Boolean
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/ButtonExample.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <btn primary>Button</btn>
+    <btn info>Button</btn>
+    <btn success>Button</btn>
+    <btn warning>Button</btn>
+    <btn danger>Button</btn>
+
+  </div>
+
+
+</template>
+
+<script>
+import Btn from "./Btn";
+export default {
+name: "ButtonExample",
+  components: {Btn}
+}
+</script>
+
+<style scoped>
+
+</style>
